77bcd5e7551cd1ec80ca1997b79cf0eeb492bf0e
Marcelo8819 <silva.marcelohg@gmail.com>
2022-11-03 20:50:31 -0300

    first commit

---
+++ app.py
@@ -0,0 +1,3 @@
+print("Hello word")
+nome = "Marcelo"
+print(nome)
---
+++ main.py
@@ -0,0 +1,8 @@
+from pessoa import Pessoa
+
+p1 = Pessoa('Luiz', 25)
+p2 = Pessoa('Pedro',35)
+
+print(p1.get_ano_nasc())
+print(p2.get_ano_nasc())
+
